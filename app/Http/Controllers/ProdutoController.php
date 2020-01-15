17f58d455f401c432dbdf0128492636dc6f8659e
Dara Vasconcelos <dcsv@cin.ufpe.br>
2020-01-15 15:41:17 -0300

    usando eloquent

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,12 +1,16 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
+use estoque\Produto;
 use Request;
 
 class ProdutoController extends Controller{
     public function lista(){
 
-        $produtos = DB::select('select * from produtos');
+       // $produtos = DB::select('select * from produtos');
+
+       //usando eloquent
+       $produtos = Produto::all();
 
         return view('produto.listagem')->with('produtos',$produtos);
 
@@ -16,11 +20,13 @@ class ProdutoController extends Controller{
       //  $id = Request::input('id','0'); //segundo parametro é valor default
         $id = Request::route('id'); //pega parametro da rota ao inves da url
        //pode remover a requeste e add $id como parametro do metodo
-        $resposta = DB::select('select * from produtos where id=?',[$id]);
-        if(empty($resposta)){
+      //  $resposta = DB::select('select * from produtos where id=?',[$id]);
+       $produto = Produto::find($id); // usando eloquent
+
+        if(empty($produto)){
             return "Esse produto não existe";
         }
-        return view('produto.detalhes')->with('p',$resposta[0]);
+        return view('produto.detalhes')->with('p',$produto);
     }
 
     public function novo(){
@@ -29,13 +35,19 @@ class ProdutoController extends Controller{
 
     public function adiciona(){
 
-        $nome = Request::input('nome');
+      /*  $nome = Request::input('nome');
         $descricao = Request::input('descricao');
         $valor = Request::input('valor');
         $quantidade = Request::input('quantidade');
 
         DB::insert('insert into produtos(nome,quantidade,valor,descricao) values(?,?,?,?)', array($nome,$quantidade,$valor,$descricao));
-        return redirect('/produtos');
+        //return redirect('/produtos')->withInput(Request::only('nome')); retorna pra uma URI*/
+
+        $params = Request::all();
+       // $produto = new Produto($params);
+      //  $produto->save();
+        Produto::create($params);
+        return redirect()->action('ProdutoController@lista')->withInput(Request::only('nome'));//retora para o metodo lista
     }
 
 }
--- resources/views/produto/listagem.blade.php
+++ resources/views/produto/listagem.blade.php
@@ -26,4 +26,12 @@
         Um ou menos itens no estoque
         </span>
     </h4>
+
+    @if(old('nome'))
+
+        <div class="alert alert-success">
+            <strong>Sucesso!</strong> o produto {{old('nome')}} foi adicionado.
+        </div>
+    @endif    
+
 @stop
